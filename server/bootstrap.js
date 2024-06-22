0341a40eae659ee1353ab8887ee24108d08733b7
abbadon1334 <fdanti@gmail.com>
2024-06-22 18:34:37 +0200

    Fix creation of static files

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,28 @@
 'use strict';
 
-module.exports = () => {
-  // bootstrap phase
+const path = require('path');
+const fs = require('fs-extra');
+const _ = require('lodash');
+
+module.exports = async () => {
+
+    const jsData = fs.readFileSync(path.resolve(__dirname, 'public', 'stripe.js'), 'utf8');
+    const filledJsData = _.template(jsData)({
+        backendUrl: strapi.config.server.url,
+    });
+    const extensionsPath = strapi.dirs.extensions || strapi.dirs.dist.extensions;
+    const bbbJsPath = path.resolve(extensionsPath, 'strapi-stripe', 'public', 'stripe.js');
+
+    setTimeout(async () => {
+        strapi.log.debug('Strapi-Stripe assets try create at:' + bbbJsPath);
+
+        await fs.ensureFile(bbbJsPath);
+        await fs.writeFile(bbbJsPath, filledJsData);
+
+        if (!fs.exists(bbbJsPath)) {
+            strapi.log.error('Error not created!!! at:' + bbbJsPath);
+        } else {
+            strapi.log.debug('File created at:' + bbbJsPath);
+        }
+    }, 1000);
 };
--- server/register.js
+++ server/register.js
@@ -10,17 +10,6 @@ const staticFileMiddleware = require('./middlewares/staticFiles');
 
 module.exports = async ({ strapi }) => {
   // registeration phase
+  console.log('Registering Strapi Stripe plugin...');
   await staticFileMiddleware({ strapi });
-  await generateJs();
-};
-
-async function generateJs() {
-  const jsData = fs.readFileSync(path.resolve(__dirname, 'public', 'stripe.js'), 'utf8');
-  const filledJsData = _.template(jsData)({
-    backendUrl: strapi.config.server.url,
-  });
-  const extensionsPath = strapi.dirs.extensions || strapi.dirs.dist.extensions;
-  const bbbJsPath = path.resolve(extensionsPath, 'strapi-stripe', 'public', 'stripe.js');
-  await fs.ensureFile(bbbJsPath);
-  await fs.writeFile(bbbJsPath, filledJsData);
 }
